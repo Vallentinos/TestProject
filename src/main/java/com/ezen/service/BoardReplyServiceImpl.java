ce16ebde939f04be31bc33287b2cabe4c7bf3ec1
hayoungK <hihihy0704@gmail.com>
2023-02-12 21:38:31 +0900

    게시판 댓글 코드 추가

--- src/main/java/com/ezen/service/BoardReplyServiceImpl.java
+++ src/main/java/com/ezen/service/BoardReplyServiceImpl.java
@@ -1,20 +1,26 @@
 package com.ezen.service;
 
+import com.ezen.entity.Board;
 import com.ezen.entity.BoardReply;
 import com.ezen.persistence.BoardReplyRepository;
+import com.ezen.persistence.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BoardReplyServiceImpl implements BoardReplyService{
 
     @Autowired
     private BoardReplyRepository boardReplyRepository;
+    @Autowired
+    private BoardRepository boardRepository;
 
     @Override
     public void insertBoardReply(BoardReply boardReply) {
+        boardRepository.findById(boardReply.getBoardReplySeq()).get();
         boardReplyRepository.save(boardReply);
     }
 
@@ -30,6 +36,7 @@ public class BoardReplyServiceImpl implements BoardReplyService{
 
     @Override
     public List<BoardReply> getBoardReplyList(BoardReply boardReply) {
-        return null;
+        
+        return boardReplyRepository.findAllByBoard_BoardSeq(boardReply.getBoard().getBoardSeq());
     }
 }
