2f7393f58adb661ec6f9408384de1f139685db6c
hayoungK <hihihy0704@gmail.com>
2023-02-16 20:49:22 +0900

    결제 페이지 수정

--- src/main/java/com/ezen/controller/PurchaseController.java
+++ src/main/java/com/ezen/controller/PurchaseController.java
@@ -10,10 +10,8 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -29,8 +27,10 @@ public class PurchaseController {
     @Autowired
     private FundingService fundingService;
 
-    @GetMapping("/purchase")
-    public String insertPurchaseForm(@SessionAttribute("member") Member member, Funding funding, Model model) {
+    @PostMapping("/purchase")
+    public String insertPurchaseForm(@SessionAttribute("member") Member member, Funding funding,
+                                     @RequestParam("p_quantity") int p_quantity, Model model) {
+
         Map<String, String> addrMap = new HashMap<>();
         String[] addressArr = null;
 
@@ -43,14 +43,15 @@ public class PurchaseController {
         addrMap.put("addr3", addressArr[2]);
 
         model.addAttribute("funding", findFunding);
+        model.addAttribute("p_quantity", p_quantity);
         model.addAttribute("member", findMember);
         model.addAttribute("address", addrMap);
 
         return "purchase/insertPurchase";
     }
 
-    @PostMapping("/purchase")
-    public String insertPurchase() {
-        return "redirect:/";
+    @PostMapping("/insertPurchase")
+    public void insertPurchase(Purchase purchase) {
+
     }
 }
